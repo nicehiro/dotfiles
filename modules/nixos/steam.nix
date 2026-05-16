ea008b5e28e1103a64ff858321bb77989572b973
fangyuan <fywang96@gmail.com>
2026-05-17 02:33:09 +0800

    Add Proton-GE to Steam compat tools

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -4,6 +4,9 @@
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
   };
 
   programs.gamemode.enable = true;
